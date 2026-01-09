3710cb8efc4c4ddeab429bf39c43b7aa0635f645
Jesse De Meulemeester <jesse.demeulemeester@esat.kuleuven.be>
2026-01-09 15:46:38 +0100

    Bump spinal and sbt version

--- project/Version.scala
+++ project/Version.scala
@@ -1,9 +1,9 @@
 object CryptoVersion {
 
   val scalaCompilers = Seq("2.12.18", "2.11.12", "2.13.6")
-  val spinal        = "1.10.2a"
+  val spinal        = "1.13.0"
 
-  private val version = "1.2.0"
+  private val version = "1.2.0-redaes"
   val tester   = s"$version"
   val crypto   = s"$version"
 }
